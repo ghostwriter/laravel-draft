c579b0efa3afd4b7e46231e664a37c00f53be93c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-11-26 21:09:15 -0600

    Update rector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- rector.php
+++ rector.php
@@ -74,15 +74,8 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::TYPE_DECLARATION_STRICT,
         SetList::EARLY_RETURN,
         SetList::PHP_81,
-        SetList::RECTOR_CONFIG,
     ]);
-    $rectorConfig->paths([
-        __DIR__ . '/compliance.php',
-        __DIR__ . '/ecs.php',
-        __DIR__ . '/rector.php',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
+    $rectorConfig->paths([__DIR__ . '/ecs.php', __DIR__ . '/rector.php', __DIR__ . '/src', __DIR__ . '/tests']);
     $rectorConfig->phpVersion(PhpVersion::PHP_80);
     $rectorConfig->skip([
         __DIR__ . '*/tests/Fixture/*',
