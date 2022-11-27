eaff69a20536505552d33080eabec78c5fe7d520
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-11-26 21:08:44 -0600

    Create DraftServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/DraftServiceProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft;
+
+use Illuminate\Support\ServiceProvider;
+
+final class DraftServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot(): void
+    {
+        /*
+         * Optional methods to load your package assets
+         */
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'draft');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'draft');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/draft.php' => config_path('draft.php'),
+            ], 'config');
+
+            // Publishing the views.
+            /*$this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/draft'),
+            ], 'views');*/
+
+            // Publishing assets.
+            /*$this->publishes([
+                __DIR__.'/../resources/assets' => public_path('vendor/draft'),
+            ], 'assets');*/
+
+            // Publishing the translation files.
+            /*$this->publishes([
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/draft'),
+            ], 'lang');*/
+
+            // Registering package commands.
+            // $this->commands([]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register(): void
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/draft.php', 'draft');
+
+        // Register the main class to use with the facade
+        $this->app->singleton(Draft::class, static fn () => new Draft());
+        $this->app->alias(Draft::class, 'draft');
+    }
+}
