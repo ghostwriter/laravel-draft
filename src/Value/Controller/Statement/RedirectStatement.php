e828ee2926c7550d328f66339eb05f2af7ef9b48
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-08 16:13:32 -0600

    Create RedirectStatement.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Controller/Statement/RedirectStatement.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Value\Controller\Statement;
+
+use Ghostwriter\Draft\Contract\Controller\StatementInterface;
+use Ghostwriter\Draft\Traits\StatementTrait;
+
+final class RedirectStatement implements StatementInterface
+{
+    use StatementTrait;
+}
