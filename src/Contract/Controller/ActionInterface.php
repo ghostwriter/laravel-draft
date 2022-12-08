7e0f1193e7f68bbf9e0828fba32379006e6cb46d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-08 16:15:28 -0600

    Create ActionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Contract/Controller/ActionInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Contract\Controller;
+
+use Ghostwriter\Draft\Contract\ModelInterface;
+
+interface ActionInterface
+{
+    public function name(): string;
+
+    public function statement(StatementInterface $statement): self;
+
+    /**
+     * @return iterable<string,StatementInterface>
+     */
+    public function statements(): iterable;
+
+    public function with(string $key, ModelInterface $value): self;
+
+    /**
+     * @param iterable<string,ModelInterface> $param
+     */
+    public function withMany(iterable $param): self;
+}
