e7e7b3bc37b245e37ce08b09dba7ca82e5df2b89
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-16 15:26:36 -0600

    Update StatementTrait.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Traits/StatementTrait.php
+++ src/Traits/StatementTrait.php
@@ -6,4 +6,45 @@ namespace Ghostwriter\Draft\Traits;
 
 trait StatementTrait
 {
+    /**
+     * @param array<string,string> $attributes
+     */
+    public function __construct(
+        private string $name,
+        private array $attributes = []
+    ) {
+    }
+
+    public function attributes(): iterable
+    {
+        yield from $this->attributes;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function with(string $key, mixed $value): self
+    {
+        $this->attributes[$key] = $value;
+
+        return $this;
+    }
+
+    public function withAttribute(string $key, string $value): self
+    {
+        $this->attributes[$key] = $value;
+
+        return $this;
+    }
+
+    public function withMany(iterable $attributes): self
+    {
+        foreach ($attributes as $key => $value) {
+            $this->attributes[$key] = $value;
+        }
+
+        return $this;
+    }
 }
