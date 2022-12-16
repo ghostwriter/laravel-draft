42766b8e59e3c66da45e477db7651fd332df452e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-16 15:26:03 -0600

    Update DispatchStatement.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Controller/Statement/DispatchStatement.php
+++ src/Value/Controller/Statement/DispatchStatement.php
@@ -10,34 +10,4 @@ use Ghostwriter\Draft\Traits\StatementTrait;
 final class DispatchStatement implements StatementInterface
 {
     use StatementTrait;
-
-    /**
-     * @param iterable<string,string> $data
-     */
-    public function __construct(
-        private string $job,
-        private iterable $data = []
-    ) {
-    }
-
-    public function data(): iterable
-    {
-        yield from $this->data;
-    }
-
-    public function dispatchJob(string $string): self
-    {
-        return $this;
-    }
-
-    public function job(): string
-    {
-        return $this->job;
-    }
-
-    public function with(iterable $data = []): self
-    {
-        $this->data = $data;
-        return $this;
-    }
 }
