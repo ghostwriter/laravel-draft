309053e3a45903e1c701483ba0833b6f55f26f7f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-17 17:56:17 -0600

    Update ModelInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/ModelInterface.php
+++ src/Contract/ModelInterface.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Draft\Contract;
 
+use Closure;
+
 interface ModelInterface
 {
+    public function migration(): MigrationInterface;
+
     public function name(): string;
 
+    public function namespace(): string;
+
     public function table(): string;
+
+    /**
+     * @param ?Closure(self,MigrationInterface):MigrationInterface $factory
+     */
+    public function withMigration(?Closure $factory = null): void;
 }
