4aaa13f6cda7256164a2fc76a4a6cbfe7b560617
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-17 17:57:39 -0600

    Update Model.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Model.php
+++ src/Value/Model.php
@@ -4,28 +4,49 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Draft\Value;
 
+use Closure;
+use Ghostwriter\Draft\Contract\MigrationInterface;
 use Ghostwriter\Draft\Contract\ModelInterface;
 use Illuminate\Database\Eloquent\Model as IlluminateModel;
 use Illuminate\Support\Str;
 
-final class Model extends IlluminateModel implements ModelInterface
+final class Model implements ModelInterface
 {
+    private ?MigrationInterface $migration = null;
+
+    private ?IlluminateModel $model = null;
+
+    private ?string $table = null;
+
     public function __construct(
-        private string $name,
-        array $attributes = []
+        private string $name
     ) {
-        $this->table = Str::of($this->name)->plural()->lower()->toString();
-        parent::__construct($attributes);
     }
 
-    public function getForeignKey(): string
+    public function controller(): string
     {
-        /** @var string $this->table */
-        return Str::of($this->table)
-            ->singular()
-            ->snake()
-            ->append('_' . $this->getKeyName())
-            ->toString();
+        return '';
+    }
+//
+//    public function getForeignKey(): string
+//    {
+//        /** @var self->table */
+//        return Str::of($this->table)
+//            ->singular()
+//            ->snake()
+//            ->append('_' . $this->getKeyName())
+//            ->toString();
+//    }
+
+    public function migration(): MigrationInterface
+    {
+        return $this->migration ??= new Migration($this);
+    }
+
+    public function model(): IlluminateModel
+    {
+        return $this->model ??= new class() extends IlluminateModel {
+        };
     }
 
     public function name(): string
@@ -33,8 +54,25 @@ final class Model extends IlluminateModel implements ModelInterface
         return $this->name;
     }
 
+    public function namespace(): string
+    {
+        return '';
+    }
+
     public function table(): string
     {
-        return $this->table;
+        return $this->table ??= Str::of($this->name)->plural()->lower()->toString();
+    }
+
+    public function withMigration(?Closure $factory = null): void
+    {
+        if (null === $factory) {
+            return;
+        }
+
+        $migration = $factory($this, $this->migration());
+        if ($migration instanceof MigrationInterface) {
+            $this->migration = $migration;
+        }
     }
 }
