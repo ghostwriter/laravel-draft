a1df4fbf4f2a65fa879e96b9b4b50916c065318d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-16 21:07:17 -0600

    Create FindModels.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Action/FindModels.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Action;
+
+use Ghostwriter\Draft\Draft;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Filesystem\Filesystem;
+use SplFileInfo;
+use Traversable;
+
+final class FindModels
+{
+    public function __construct(
+        private Draft $draft,
+        private Filesystem $filesystem
+    ) {
+    }
+
+    /**
+     * @throws FileNotFoundException
+     */
+    public function __invoke(): iterable
+    {
+        foreach ($this->filesystem->files($this->draft->modelPath()) as $controller) {
+            $path = $controller->getRealPath();
+
+            yield $path => $this->draft->parse($this->filesystem->get($path), $controller->getFilename());
+        }
+    }
+}
