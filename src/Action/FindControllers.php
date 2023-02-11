2ebd1fa5a33f39618f99c00cb28f5821df0d59a1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-02-11 17:55:37 -0600

    Update FindControllers.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Action/FindControllers.php
+++ src/Action/FindControllers.php
@@ -4,27 +4,26 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Draft\Action;
 
+use Generator;
 use Ghostwriter\Draft\Draft;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
+use PhpParser\Node\Stmt;
 
 final class FindControllers
 {
-    public function __construct(
-        private Draft $draft,
-        private Filesystem $filesystem
-    ) {
-    }
-
     /**
+     * @return Generator<string,array<Stmt>>
      * @throws FileNotFoundException
      */
-    public function __invoke(): iterable
+    public function __invoke(Draft $draft, Filesystem $filesystem): Generator
     {
-        foreach ($this->filesystem->files($this->draft->controllerPath()) as $controller) {
+        foreach ($filesystem->files($draft->controllerPath()) as $controller) {
             $path = $controller->getRealPath();
-
-            yield $path => $this->draft->parse($this->filesystem->get($path), $controller->getFilename());
+            if ($path === false) {
+                throw new FileNotFoundException();
+            }
+            yield $path => $draft->parse($filesystem->get($path), $controller->getFilename());
         }
     }
 }
