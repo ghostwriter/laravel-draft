08e839fa8d21b24870e994c7a3a765c2fe1f90fc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-02-12 18:10:02 -0600

    Update ControllerVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Visitor/ControllerVisitor.php
+++ src/Visitor/ControllerVisitor.php
@@ -5,9 +5,34 @@ declare(strict_types=1);
 namespace Ghostwriter\Draft\Visitor;
 
 use Ghostwriter\Draft\Visitor\Traits\NodeVisitorTrait;
+use PhpParser\Node;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Stmt\Class_;
 use PhpParser\NodeVisitor;
 
 final class ControllerVisitor implements NodeVisitor
 {
     use NodeVisitorTrait;
+
+    private ?Identifier $name = null;
+
+    private ?Node $node = null;
+
+    public function enterNode(Node $node): mixed
+    {
+        if (! $node instanceof Class_) {
+            return null;
+        }
+
+        $this->node = $node;
+
+        $nodeName = $node->name;
+        if (null === $nodeName) {
+            return null;
+        }
+
+        $this->name = $nodeName;
+
+        return null;
+    }
 }
