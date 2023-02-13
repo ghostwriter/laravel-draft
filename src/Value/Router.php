cf3a814adf3b59cc5e77a5e0fc7ed778749456b2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-02-12 18:11:25 -0600

    Update Router.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Router.php
+++ src/Value/Router.php
@@ -9,4 +9,13 @@ use Illuminate\Routing\Router as IlluminateRouter;
 
 final class Router extends IlluminateRouter implements RouterInterface
 {
+    public function user(): UserInterface
+    {
+        $user = $this->user;
+        if ($user instanceof UserInterface) {
+            return $user;
+        }
+
+        throw new RuntimeException('No user was provided.');
+    }
 }
