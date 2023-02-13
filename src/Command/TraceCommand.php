a336da7635b979d8737e5ba3df60dea889af9aca
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-02-12 18:12:25 -0600

    Update TraceCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/TraceCommand.php
+++ src/Command/TraceCommand.php
@@ -4,34 +4,119 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Draft\Command;
 
+use Closure;
 use Ghostwriter\Draft\Action\FindControllers;
 use Ghostwriter\Draft\Action\FindModels;
 use Ghostwriter\Draft\Draft;
 use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
+use PhpParser\Node\ComplexType;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Property;
+use PhpParser\NodeFinder;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor\NameResolver;
 
-final class TraceCommand extends Command
+final class TraceCommand extends GeneratorCommand
 {
-    protected $description = 'Trace the full project for models';
+    protected $description = 'Trace your project to generate a "draft.php" file.';
 
     protected $signature = 'draft:trace';
 
     public function __construct(
-        private Draft $draft,
-        private Filesystem $filesystem
+        private readonly Draft $draft,
+        private readonly Filesystem $filesystem,
+        private readonly FindModels $findModels,
+        private readonly FindControllers $findControllers
     ) {
-        parent::__construct();
+        parent::__construct($this->filesystem);
     }
 
     /**
      * Execute the console command.
+     *
+     * @throws FileNotFoundException
      */
     public function handle(): int
     {
-        $models =
-        iterator_to_array((new FindModels($this->draft, $this->filesystem))());
-        $controllers =
-        iterator_to_array((new FindControllers($this->draft, $this->filesystem))());
+        // Ask laravel where to find the model/controller/migrations/factories/seeders/formRequests/Notification dir
+
+        [$models, $timeAndMemory_0] = $this->bench(fn() => ($this->findModels)($this->draft, $this->filesystem));
+
+        [$controllers, $timeAndMemory_1] = $this->bench(fn() => ($this->findControllers)($this->draft, $this->filesystem));
+
+//        $dump = [];
+//        [,$testMem] = $this->bench(function () use ($dump) {
+//            foreach (range(0, 100000) as $index){
+//                $dump[$index] = time();
+//            }
+//            return $dump;
+//        });
+dd([
+    iterator_to_array($models),
+    iterator_to_array($controllers),
+
+    $timeAndMemory_0,
+    $timeAndMemory_1,
+//    $testMem ?? null
+]);
+        die;
+//         =
+
+        /** @var array<Class_> $classes */
+        dd([
+            $models,
+            array_keys($models),
+//            array_values($models),
+//$this->draft->traverse($models[array_key_last($models)]),
+
+//            $this->qualifyClass($model),
+//            $this->qualifyModel($model),
+//$this->rootNamespace()
+//
+//            iterator_to_array($findModels($this->draft, $this->filesystem)),
+//            iterator_to_array($findControllers($this->draft, $this->filesystem)),
+//            //            $this->filesystem->files(app()->databasePath('app/models')),
+//            //        ??
+//            app()
+//                ->getNamespace(), // "app" or "custom if changed by user"
+//            app()
+//                ->getNamespace() . 'Actions',
+//            app()
+//                ->getNamespace() . 'Models',
+//            app()
+//                ->getNamespace() . 'Events',
+//            app()
+//                ->getNamespace() . 'Policies',
+//            app()
+//                ->getNamespace() . 'Views',
+//            app()
+//                ->getNamespace() . 'Http\\Controllers',
+//
+//            //            app()->basePath('Model'),
+            //            app()->databasePath()
+        ]);
+
+//        \Illuminate\Foundation\Application::class;
+
+        // Using PHP parser load each of the file paths/classNames in to a classmap
+
+        // Using the classmap, determine which files are missing (based on a criteria/strategy)
+
+        // User can enable/disable a "criteria/strategy" in `config/draft.php`
+
+        //
+//        $models =
+//        iterator_to_array((new FindModels($this->draft, $this->filesystem))());
+//        $controllers =
+//        iterator_to_array((new FindControllers($this->draft, $this->filesystem))());
 
         //            $this->callSilently('queue:monitor', []);
 
@@ -60,5 +145,26 @@ final class TraceCommand extends Command
         //        ]);
 
         return self::SUCCESS;
+    }
+
+    protected function getStub(): void
+    {
+    }
+
+    protected function bench(Closure $fn): array
+    {
+        $start = -hrtime(true)/1e+6;
+        $memoryStart = -memory_get_usage(true);
+
+        /** @var array<Stmt> $result */
+        $result = $fn();
+
+        $end = hrtime(true)/1e+6;
+        $memoryEnd = memory_get_usage(true);
+
+        return [
+            $result,
+            sprintf('Time: %f - Memory: %s', $start + $end, $memoryEnd + $memoryStart),
+        ];
     }
 }
