2fbaa6662453d592cf25dd9ff2b364395a0518fe
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-02-12 18:11:31 -0600

    Update MigrationVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Visitor/MigrationVisitor.php
+++ src/Visitor/MigrationVisitor.php
@@ -5,9 +5,24 @@ declare(strict_types=1);
 namespace Ghostwriter\Draft\Visitor;
 
 use Ghostwriter\Draft\Visitor\Traits\NodeVisitorTrait;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
 use PhpParser\NodeVisitor;
 
 final class MigrationVisitor implements NodeVisitor
 {
     use NodeVisitorTrait;
+
+    private ?Class_ $node = null;
+
+    public function enterNode(Node $node): mixed
+    {
+        if (! $node instanceof Class_) {
+            return null;
+        }
+
+        $this->node = $node;
+
+        return null;
+    }
 }
