6641d1362c575ed9808145d3e72d765317664b76
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-02-12 18:10:12 -0600

    Update NodeVisitorTrait.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Visitor/Traits/NodeVisitorTrait.php
+++ src/Visitor/Traits/NodeVisitorTrait.php
@@ -42,19 +42,29 @@ trait NodeVisitorTrait
     }
 
     /**
+     * If NodeVisitor::enterNode() returns DONT_TRAVERSE_CHILDREN, child nodes of the current node will not be traversed
+     * for any visitors.
+     *
+     * For subsequent visitors NodeVisitor::enterNode() will still be called on the current node and
+     * NodeVisitor::leaveNode() will also be invoked for the current node.
+     */
+    public static function dontTraverseChildren(): int
+    {
+        return NodeTraverser::DONT_TRAVERSE_CHILDREN;
+    }
+
+    /**
      * Called when entering a node.
      *
      * Return value semantics:
      *  - null => $node stays as-is
-     *  - NodeTraverser::DONT_TRAVERSE_CHILDREN => Children of $node are not traversed. $node stays as-is
-     *  - NodeTraverser::STOP_TRAVERSAL => Traversal is aborted. $node stays as-is
+     *  - NodeTraverser::dontTraverseChildren() => Children of $node are not traversed. $node stays as-is
+     *  - NodeTraverser::stopTraversal() => Traversal is aborted. $node stays as-is
      *  - otherwise => $node is set to the return value
      *
      * @param Node $node Node
-     *
-     * @return null|int|Node Replacement node (or special return value)
      */
-    public function enterNode(Node $node): mixed
+    public function enterNode(Node $node): null|int|Node
     {
         return null;
     }
@@ -64,8 +74,8 @@ trait NodeVisitorTrait
      *
      * Return value semantics:
      *  - null => $node stays as-is
-     *  - NodeTraverser::REMOVE_NODE => $node is removed from the parent array
-     *  - NodeTraverser::STOP_TRAVERSAL => Traversal is aborted. $node stays as-is
+     *  - NodeTraverser::removeNode() => $node is removed from the parent array
+     *  - NodeTraverser::stopTraversal() => Traversal is aborted. $node stays as-is
      *  - array (of Nodes) => The return value is merged into the parent array (at the position of the $node)
      *  - otherwise => $node is set to the return value
      *
@@ -78,9 +88,42 @@ trait NodeVisitorTrait
         return null;
     }
 
+    /**
+     * If NodeVisitor::leaveNode() returns REMOVE_NODE for a node that occurs in an array, it will be removed from the
+     * array.
+     *
+     * For subsequent visitors leaveNode() will still be invoked for the removed node.
+     */
+    public static function removeNode(): int
+    {
+        return NodeTraverser::REMOVE_NODE;
+    }
+
+    /**
+     * If NodeVisitor::enterNode() or NodeVisitor::leaveNode() returns STOP_TRAVERSAL, traversal is aborted.
+     *
+     * The afterTraverse() method will still be invoked.
+     */
+    public static function stopTraversal(): int
+    {
+        return NodeTraverser::STOP_TRAVERSAL;
+    }
+
     public function traverse(array $nodes, ?NodeTraverser $nodeTraverser = null): iterable
     {
         yield from ($nodeTraverser ??= new NodeTraverser())
             ->traverse($nodes);
+    }
+
+    /**
+     * If NodeVisitor::enterNode() returns DONT_TRAVERSE_CURRENT_AND_CHILDREN, child nodes of the current node will not
+     * be traversed for any visitors.
+     *
+     * For subsequent visitors enterNode() will not be called as well. leaveNode() will be invoked for visitors that has
+     * enterNode() method invoked.
+     */
+    protected static function dontTraverseCurrentAndChildren(): int
+    {
+        return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
     }
 }
