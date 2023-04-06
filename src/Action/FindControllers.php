c0c1772e7e9342a951485e31d2d453294f07ac41
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-05 19:48:15 -0500

    Update FindControllers.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Action/FindControllers.php
+++ src/Action/FindControllers.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace Ghostwriter\Draft\Action;
 
 use Generator;
+use Ghostwriter\Draft\ClassMap;
 use Ghostwriter\Draft\Draft;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
-use PhpParser\Node;
-use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassConst;
@@ -21,15 +20,16 @@ use PhpParser\NodeVisitor\NameResolver;
 final class FindControllers
 {
     /**
-     * @var array<Node>
+     * @var array<string,array<string>>
      */
     private array $controllers = [];
+
     /**
      * @throws FileNotFoundException
      *
      * @return Generator<string,array<Stmt>>
      */
-    public function __invoke(Draft $draft, Filesystem $filesystem): Generator
+    public function __invoke(Draft $draft, Filesystem $filesystem, ClassMap $classMap): Generator
     {
         $traverser = new NodeTraverser();
         $nameResolver = new NameResolver();
@@ -46,39 +46,41 @@ final class FindControllers
 
             /** @var array<Class_> $classes */
             $classes = $nodeFinder->findInstanceOf($nodes, Class_::class);
-            foreach ($classes as $class){
+            foreach ($classes as $class) {
                 $className = NodeExtractor::getName($class);
+                $classMap->addClass($className, $path);
 
                 /** @var array<ClassConst> $constants */
                 $constants = $nodeFinder->findInstanceOf($class, ClassConst::class);
-                foreach ($constants as $constant){
-                    $constName = NodeExtractor::getName($constant);
-                    $constValue = NodeExtractor::getValue($constant);
-
-                    $this->controllers[$path][$className]['constant'][$constName] = $constValue;
+                foreach ($constants as $constant) {
+                    $constant = NodeExtractor::getConsts($constant);
+                    /** @var array<string,array<string>> $current */
+                    $current =&$this->controllers[$path][$className]['constant'];
+                    array_push($current, $constant);
                 }
 
                 /** @var array<ClassMethod> $methods */
                 $methods = $nodeFinder->findInstanceOf($class, ClassMethod::class);
-                foreach ($methods as $method){
+                foreach ($methods as $method) {
                     $methodName = NodeExtractor::getName($method);
                     $methodParams = NodeExtractor::getParams($method);
-                    $this->controllers[$path][$className]['methods'][$methodName] = $methodParams;
+
+                    assert(null !== $methodName);
+
+                    $this->controllers[$path][$className]['methods'] = [
+                        $methodName => $methodParams,
+                    ];
                 }
 
-
-//                $this->controllers[$path][$className][] = true;
-
-
+                //                $this->controllers[$path][$className][] = true;
             }
-//            yield $path => $draft->parse($filesystem->get($path), $controller->getFilename());
-//            $this->controllers[$path] = $ast;
-//             [$className]['constant'][sprintf('%s::%s', $className, $constName)]
-//                 = $constValue;
+            //            yield $path => $draft->parse($filesystem->get($path), $controller->getFilename());
+            //            $this->controllers[$path] = $ast;
+            //             [$className]['constant'][sprintf('%s::%s', $className, $constName)]
+            //                 = $constValue;
         }
-
-//        dd($this);
-        yield from $this->controllers;
+        yield $classMap;
+        //        yield from $this->controllers;
     }
 
     /**
